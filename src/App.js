642b0ad5b418feb38f275668f2a8c2e65e400c1c
Omoefe Joseph Ugboma <jkomoefey23e@yahoo.co.uk>
2020-06-01 21:34:04 +0100

    Adding a GlobalState to fetch transaction component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header/>
       <div className="container">
         <Balance/>
@@ -16,7 +17,7 @@ function App() {
         <TransactionList/>
         <AddTransaction/>
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
