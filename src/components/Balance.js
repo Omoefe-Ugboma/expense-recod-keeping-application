c9e0a000022aad5d62ebce3fddaed58ce3a63557
Omoefe Joseph Ugboma <jkomoefey23e@yahoo.co.uk>
2020-06-01 23:20:32 +0100

    Deleting of component

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,15 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 export const Balance = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
     return (
         <div>
             <h4>Your Balance</h4>
-            <h1>$0.00</h1>
+            <h1>₦{total}</h1>
         </div>
     )
 }
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
+
 import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
-    const context = useContext(GlobalContext);
+    const { transactions } = useContext(GlobalContext);
 
-    console.log(context);
+    
     return (
         <div>
             <h3>History</h3>
             <ul className="list">
-                <li className="minus">
-                Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li>
+                {transactions.map(transaction =>(<Transaction key={transaction.id} transaction={transaction}/>))}   
             </ul>
         </div>
     )
