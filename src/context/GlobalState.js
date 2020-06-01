09bff6ac55db486b004c5d4fa9cbefaf1a01f6ab
Omoefe Joseph Ugboma <jkomoefey23e@yahoo.co.uk>
2020-06-02 00:22:12 +0100

    Complete project on Expense Tracker with images

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,13 +2,9 @@ import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
 // Initial state
 const initialState = {
-    transactions: [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
-    ]
-}
+     transactions: []
+    
+ }
 
 // Create context
 export const GlobalContext = createContext(initialState);
@@ -17,7 +13,7 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) =>{
    const [state, dispatch] = useReducer(AppReducer, initialState);
 
-//    Actions
+//Actions
 function deleteTransaction(id){
     dispatch({
         type: 'DELETE_TRANSACTION',
@@ -25,9 +21,19 @@ function deleteTransaction(id){
     });
 }
 
+//Actions
+function addTransaction(transaction){
+    dispatch({
+        type: 'ADD_TRANSACTION',
+        payload: transaction,
+        
+    });
+}
+
    return (<GlobalContext.Provider value={{
        transactions: state.transactions,
-       deleteTransaction
+       deleteTransaction,
+       addTransaction
    }}>
        {children}
      </GlobalContext.Provider>);
