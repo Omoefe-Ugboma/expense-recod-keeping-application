824f6edffcd3df05ac64ac12b5ffda03e9f5ad11
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2021-01-02 09:32:43 +0100

    Updates

---
+++ precache-manifest.b510fe56c0e2284a3cbfecc8635cae13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9f227d18f82daeda831bf0091de0f3",
+    "url": "/expense-recod-keeping-application/index.html"
+  },
+  {
+    "revision": "b57ed185f1219a04ed83",
+    "url": "/expense-recod-keeping-application/static/css/main.2a77bc46.chunk.css"
+  },
+  {
+    "revision": "52be1f5d5c695c93f168",
+    "url": "/expense-recod-keeping-application/static/js/2.425c6326.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-recod-keeping-application/static/js/2.425c6326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b57ed185f1219a04ed83",
+    "url": "/expense-recod-keeping-application/static/js/main.ed835251.chunk.js"
+  },
+  {
+    "revision": "323565b7b67bb66454be",
+    "url": "/expense-recod-keeping-application/static/js/runtime-main.d7af303d.js"
+  }
+]);
